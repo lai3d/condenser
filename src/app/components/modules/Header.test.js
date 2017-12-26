962f5b51048d5a66486dea0b7d1c576fd2243744
Benjamin Chodoroff <bc@thermitic.net>
2017-12-26 16:56:25 -0500

    wip

--- src/app/components/modules/Header.test.js
+++ src/app/components/modules/Header.test.js
@@ -2,7 +2,11 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+
 import { _Header_ } from './Header';
+
+Enzyme.configure({ apapter: new Adapter() });
 
 describe('Header', () => {
     it('contains class .header', () => {
